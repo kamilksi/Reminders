15204357b6179ea11540d2649d75132cedcdd9b5
kamilksi <kamilksiazek002@gmail.com>
2022-08-18 18:54:08 +0200

    initial commit

---
+++ Reminders/Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Kamil Książek on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
