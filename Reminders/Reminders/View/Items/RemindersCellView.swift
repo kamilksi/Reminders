d3fe67d39a0435a2ba946819e78061661a7241d6
kamilksi <kamilksiazek002@gmail.com>
2022-08-18 19:38:09 +0200

    Reminders cell view

---
+++ Reminders/Reminders/View/Items/RemindersCellView.swift
@@ -0,0 +1,20 @@
+//
+//  RemindersCellView.swift
+//  Reminders
+//
+//  Created by Kamil Książek on 18/08/2022.
+//
+
+import SwiftUI
+
+struct RemindersCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RemindersCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        RemindersCellView()
+    }
+}
