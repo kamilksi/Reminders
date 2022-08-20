29d08aeaa8bb22d75fd33b95e6618bc7c800d9ea
kamilksi <kamilksiazek002@gmail.com>
2022-08-20 22:56:15 +0200

    refactor main view

--- Reminders/Reminders/View/Feed/FeedView.swift
+++ Reminders/Reminders/View/Feed/FeedView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-            List{
-                Section(header: Text("My lists").font(.title3).fontWeight(.black).foregroundColor(.black)){
+        List{
+            Section(header: Text("My lists").font(.title3).fontWeight(.black).foregroundColor(.black)){
                 ForEach(0 ..< 5) { item in
                     NavigationLink {
                         RemindersCellView()
